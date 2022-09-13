c61d94e765d05bcfb6560d2acffba57e6d28aa41
Nauris Linde <naurislinde@gmail.com>
2022-09-13 14:13:10 +0300

    fix: sitemap access and base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 import generateSitemap from 'vite-ssg-sitemap'
@@ -10,99 +10,120 @@ import { VitePWA } from 'vite-plugin-pwa'
 import Inspect from 'vite-plugin-inspect'
 import Unocss from 'unocss/vite'
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      '~/': `${path.resolve(__dirname, 'src')}/`,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    resolve: {
+      alias: {
+        '~/': `${path.resolve(__dirname, 'src')}/`,
+      },
     },
-  },
-  plugins: [
-    Vue({
-      reactivityTransform: true,
-    }),
+    plugins: [
+      Vue({
+        reactivityTransform: true,
+      }),
 
-    // https://github.com/hannoeru/vite-plugin-pages
-    Pages(),
+      // https://github.com/hannoeru/vite-plugin-pages
+      Pages(),
 
-    // https://github.com/JohnCampionJr/vite-plugin-vue-layouts
-    Layouts(),
+      // https://github.com/JohnCampionJr/vite-plugin-vue-layouts
+      Layouts(),
 
-    // https://github.com/antfu/unplugin-auto-import
-    AutoImport({
-      imports: [
+      // https://github.com/antfu/unplugin-auto-import
+      AutoImport({
+        imports: [
+          'vue',
+          'vue-router',
+          'vue/macros',
+          '@vueuse/head',
+          '@vueuse/core',
+        ],
+        dts: 'src/auto-imports.d.ts',
+        dirs: [
+          'src/composables',
+        ],
+        vueTemplate: true,
+      }),
+
+      // https://github.com/antfu/unplugin-vue-components
+      Components({
+        dts: 'src/components.d.ts',
+      }),
+
+      // https://github.com/antfu/unocss
+      // see unocss.config.ts for config
+      Unocss(),
+
+      // https://github.com/antfu/vite-plugin-pwa
+      VitePWA({
+        registerType: 'autoUpdate',
+        includeAssets: ['favicon.svg', 'robots.txt', 'sitemap.xml', 'safari-pinned-tab.svg'],
+        manifest: {
+          name: env.VITE_APP_TITLE,
+          short_name: env.VITE_SHORT_NAME,
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: '/pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: '/pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+            {
+              src: '/pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable',
+            },
+          ],
+        },
+      }),
+
+      // https://github.com/antfu/vite-plugin-inspect
+      // Visit http://localhost:3333/__inspect/ to see the inspector
+      Inspect(),
+    ],
+
+    // https://github.com/vitest-dev/vitest
+    test: {
+      include: ['test/**/*.test.ts'],
+      environment: 'jsdom',
+      deps: {
+        inline: ['@vue', '@vueuse', 'vue-demi'],
+      },
+    },
+
+    // https://github.com/antfu/vite-ssg
+    ssgOptions: {
+      script: 'async',
+      formatting: 'minify',
+      dirStyle: 'nested',
+      onFinished() {
+        generateSitemap({
+          hostname: env.VITE_BASE_URL,
+        })
+      },
+    },
+
+    optimizeDeps: {
+      include: [
         'vue',
         'vue-router',
-        'vue/macros',
-        '@vueuse/head',
         '@vueuse/core',
+        '@vueuse/head',
       ],
-      dts: 'src/auto-imports.d.ts',
-      dirs: [
-        'src/composables',
+      exclude: [
+        'vue-demi',
       ],
-      vueTemplate: true,
-    }),
+    },
 
-    // https://github.com/antfu/unplugin-vue-components
-    Components({
-      dts: 'src/components.d.ts',
-    }),
-
-    // https://github.com/antfu/unocss
-    // see unocss.config.ts for config
-    Unocss(),
-
-    // https://github.com/antfu/vite-plugin-pwa
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'robots.txt', 'safari-pinned-tab.svg'],
-      manifest: {
-        name: process.env.VITE_APP_TITLE,
-        short_name: process.env.VITE_SHORT_NAME,
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: '/pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-      },
-    }),
-
-    // https://github.com/antfu/vite-plugin-inspect
-    // Visit http://localhost:3333/__inspect/ to see the inspector
-    Inspect(),
-  ],
-
-  // https://github.com/antfu/vite-ssg
-  ssgOptions: {
-    script: 'async',
-    formatting: 'minify',
-    dirStyle: 'nested',
-    onFinished() { generateSitemap() },
-  },
-
-  optimizeDeps: {
-    include: [
-      'vue',
-      'vue-router',
-      '@vueuse/core',
-      '@vueuse/head',
-    ],
-    exclude: [
-      'vue-demi',
-    ],
-  },
+    ssr: {
+      // TODO: workaround until they support native ESM
+      noExternal: ['workbox-window'],
+    },
+  }
 })
