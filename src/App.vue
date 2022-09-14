683a908064ae465e7de9207bcb7ebee5f6dfe004
Nauris Linde <naurislinde@gmail.com>
2022-09-14 21:32:48 +0300

    fix: copy .env.template to .env in test-e2e ci job

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 import { isDark, preferredDark } from '~/composables'
-import { removeTrailingSlash } from '~/helpers'
 
 useHead({
   title: 'Nauris Linde',
