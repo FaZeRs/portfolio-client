6afd5b65c900ef8e359782c8a881ad7dfc3af546
Nauris Linde <naurislinde@gmail.com>
2022-09-20 15:02:44 +0300

    fix: disabled pwa typecheck

--- src/modules/pwa.ts
+++ src/modules/pwa.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import { type UserModule } from '~/types'
 
 // https://github.com/antfu/vite-plugin-pwa#automatic-reload-when-new-content-available
