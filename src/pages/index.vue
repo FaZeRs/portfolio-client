d37d63ae26d2f574670393a28dfba3234f6f2179
Nauris Linde <naurislinde@gmail.com>
2023-03-06 22:36:39 +0200

    fix: updated index page about me section

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -161,7 +161,13 @@ useHead({
             I'm Nauris Linde, a Backend Developer
           </h4>
           <p class="pt-6 font-body leading-relaxed text-slate-600 dark:text-slate-300">
-            Currently I'm working as C++ Developer at Giraffe360. I have been programming for 7 years.
+            I am a C++ developer with expertise in desktop app and full-stack web dev. I create high-quality software using various skills and knowledge.
+          </p>
+          <p class="pt-6 font-body leading-relaxed text-slate-600 dark:text-slate-300">
+            My C++ skills enable me to design complex and efficient desktop apps for Windows, Linux, and macOS. I use C++ libraries and frameworks like Qt, OpenCV, and STL to optimize code and build scalable applications.
+          </p>
+          <p class="pt-6 font-body leading-relaxed text-slate-600 dark:text-slate-300">
+            I am proficient in full-stack web dev using NodeJS, Typescript, VueJS, and NestJS. I develop e-commerce platforms, dashboards, and inventory management systems.
           </p>
         </div>
       </div>
